53d5cf1f53453ce0fad6c8c9451e97c082bb2bdb
Mr-IoT <veeru.rockstar249@gmail.com>
2020-04-14 21:44:45 +0530

    Update tools_installation.sh

--- tools_installation.sh
+++ tools_installation.sh
@@ -48,5 +48,16 @@ sudo npm install gattacker
 echo ----------BETTERCAP------------------
 sudo apt-get install build-essential ruby-dev libpcap-dev
 sudo apt-get install build-essential libpcap-dev net-tools
-   
+wget https://github.com/bettercap/bettercap/releases/download/v2.27.1/bettercap_linux_amd64_v2.27.1.zip
+#unzip bettercap_linux_amd64_v2.27.1.zip
+#cd bettercap_linux_amd64_v2.27.1
+#mv bettercap /usr/bin
 
+
+echo -------------NRF Connect--------------
+#wget
+
+echo -------- Sniffle-----------
+wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-linux.tar.bz2?revision=c34d758a-be0c-476e-a2de-af8c6e16a8a2?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Linux,8-2019-q3-update
+
+git clone https://github.com/nccgroup/Sniffle.git
