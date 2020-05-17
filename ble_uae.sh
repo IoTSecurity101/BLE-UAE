edb69697682c16ba924281583c07b7b258a1e0e5
Mr-IoT <veeru.rockstar249@gmail.com>
2020-05-17 20:26:58 +0530

    Update ble_uae.sh

--- ble_uae.sh
+++ ble_uae.sh
@@ -62,3 +62,16 @@ echo -------- Sniffle-----------
 wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2019q3/RC1.1/gcc-arm-none-eabi-8-2019-q3-update-linux.tar.bz2?revision=c34d758a-be0c-476e-a2de-af8c6e16a8a2?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Linux,8-2019-q3-update
 
 git clone https://github.com/nccgroup/Sniffle.git
+
+
+echo "                         **                 **      **
+                        /**                //      /**
+  ******   *******      /** ******  ******  **     /**
+ //////** //**///**  ******//**//* **////**/**  ******
+  *******  /**  /** **///** /** / /**   /**/** **///**
+ **////**  /**  /**/**  /** /**   /**   /**/**/**  /**
+//******** ***  /**//******/***   //****** /**//******
+ //////// ///   //  ////// ///     //////  //  ////// "
+ 
+ 
+sudo apt-get install apktool && pip3 install -y frida
